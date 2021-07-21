1abd82e522c7d1f6a8363fcf7479bfbe599aa07e
wilyanto <wilyanto24@gmail.com>
2021-07-21 12:34:31 +0700

    adding auth bloc events and states

---
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:weather_app/domain/auth/auth_failure.dart';
+import 'package:weather_app/domain/auth/value_objects.dart';
+
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+part 'sign_in_form_bloc.freezed.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  SignInFormBloc() : super(SignInFormState.initial());
+
+  @override
+  Stream<SignInFormState> mapEventToState(
+    SignInFormEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
